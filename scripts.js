e244231b2afad197f497a999f09ca03dac481bf6
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-05 16:00:20 +0200

    transform provinces array and return new array with capitalized provinces using map method

--- scripts.js
+++ scripts.js
@@ -4,12 +4,16 @@ const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', '
 // A list of names:
 const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie']
 
-//exercise 1:
+//exercise 1 - forEach Basics:
 provinces.forEach((province, index) => {
   const name = names[index];
   console.log(name, province);
 });
 //Ask Joshua- should Province be in brackets? e.g Ashwin (WesternCape)?
+
+//exercise 2 - Uppercase Transformation: 
+const uppercasedProvinces = provinces.map(province => province.toUpperCase())
+console.log(uppercasedProvinces);
 
 
 // A list of products with prices:
